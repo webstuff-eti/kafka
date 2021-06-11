5366a0a5d3c3d07524ad53be9dd400941f884eff
Tiago <tibaestiago@gmail.com>
2021-06-11 15:40:23 -0300

    adicionando anotações do Lombok

--- api-kafka/src/main/java/com/tibaestiago/apikafka/model/AbstractModel.java
+++ api-kafka/src/main/java/com/tibaestiago/apikafka/model/AbstractModel.java
@@ -1,32 +1,14 @@
 package com.tibaestiago.apikafka.model;
 
+import lombok.*;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AbstractModel{
 
     private String title;
     private String description;
-
-    public AbstractModel(String title, String description) {
-        this.title = title;
-        this.description = description;
-    }
-
-    public AbstractModel(){}
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
     @Override
     public String toString() {
--- api-kafka/src/main/java/com/tibaestiago/apikafka/model/FieldModel.java
+++ api-kafka/src/main/java/com/tibaestiago/apikafka/model/FieldModel.java
@@ -1,33 +1,20 @@
 package com.tibaestiago.apikafka.model;
 
+import lombok.*;
+
 import java.io.Serializable;
 
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FieldModel implements Serializable {
 
     private String nome;
     private String sobrenome;
 
-    public FieldModel(){}
-
-    public FieldModel(String nome, String sobrenome){
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getSobrenome() {
-        return sobrenome;
-    }
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
+    @Override
     public String toString(){
         return nome + " " + sobrenome + "\n";
     }
+
 }
